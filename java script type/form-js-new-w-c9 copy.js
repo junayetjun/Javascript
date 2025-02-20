fbca78d65f222f678e9cc6b020078b834fcc1b5e
junayetjun <junayet03@gmail.com>
2025-02-20 19:15:28 +0600

    20-02-25
    exam

---
+++ java script type/form-js-new-w-c9 copy.js
@@ -0,0 +1,7 @@
+
+let id = document.getElementById("id").value;
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let gender = document.("email").value;
+let email = document.getElementById("email").value;
+
