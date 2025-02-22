fb32669a81d6d8421785fd962f2ec55fe343178b
junayetjun <junayet03@gmail.com>
2025-02-22 18:44:31 +0600

    22-02-25
    practice java form-email-factorial number

--- java script type/form-js-new-w-c9 copy.js
+++ java script type/form-js-new-w-c9 copy.js
@@ -1,7 +1,24 @@
 
-let id = document.getElementById("id").value;
-let name = document.getElementById("name").value;
+function formDataSend(){
+    let id = document.getElementById("id").value;
+let userName = document.getElementById("name").value;
 let email = document.getElementById("email").value;
-let gender = document.("email").value;
-let email = document.getElementById("email").value;
+let gender = document.querySelector("input[name='gender']:checked");
+let hobby = document.querySelectorAll("input[name='hobby']:checked");
+let subject = document.getElementById("subject").value;
 
+
+let hobbyArray= []
+
+for(let i=0; i<hobby.length; i++){
+    hobbyArray.push(hobby[i].value);
+
+
+}
+let output = "Id: "+ id+"\n"+ "Name: "+ userName+ "\n"+ "Email: "
++ email+ "\n"+ "Gender: "+gender.value+"\n"+ "Hobby: "+ hobbyArray+"\n"+ "Subject: "+subject;
+
+
+let newWindow =window.open("","_blank");
+newWindow.document.write("<pre>"+ output+ "</pre>");
+}
